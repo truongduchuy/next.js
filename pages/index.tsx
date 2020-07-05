59a76ee52dfc79f73f0d0ccdeb8474caefe73457
Huy Truong <huy.truong@mevry.com>
2020-07-05 15:01:22 +0700

    convert code into typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,18 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import { GetStaticProps} from 'next'
 
-export default function Home({allPostsData }) {
-  console.log('allPostsData', allPostsData)
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
+  // console.log('allPostsData', allPostsData)
   return (
     <Layout home>
       <Head>
@@ -40,7 +49,7 @@ export default function Home({allPostsData }) {
   )
 }
 
-export async function getStaticProps() {
+export const getStaticProps : GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
